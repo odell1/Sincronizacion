88e9f41862ba28aa7b7a8ca90733444a886381e8
odell1 <pablo.lledo@gmail.com>
2025-01-14 14:35:49 +0100

    ad

--- src/Ejermplo9/HiloCirculo.java
+++ src/Ejermplo9/HiloCirculo.java
@@ -1,11 +1,12 @@
 package Ejermplo9;
 
+import java.util.List;
 import java.util.Random;
 
 
 public class HiloCirculo implements Runnable {
 
-    private VentanaCirculos panel;
+ /*   private VentanaCirculos panel;
     private int numeroCirculo;
 
     public HiloCirculo(VentanaCirculos panel, int numeroCirculo) {
@@ -28,6 +29,50 @@ public class HiloCirculo implements Runnable {
             }
         }
     }
+}*/
+
+private VentanaCirculos panel;
+    private int numeroCirculo;
+    private static final int[] COLORES = {1, 2, 3, 4, 5};
+    private static boolean[] coloresUsados = new boolean[COLORES.length];
+    
+
+    public HiloCirculo(VentanaCirculos panel, int numeroCirculo) {
+        this.panel = panel;
+        this.numeroCirculo = numeroCirculo;
+    }
+
+    @Override
+    public void run() {
+        
+            synchronized (COLORES) {
+               if(todosSonTrue(coloresUsados)){
+                    panel.parpadearCirculo(numeroCirculo, 500);
+               }else{
+                    int colorAsignado;
+                    do {
+                        colorAsignado = COLORES[new Random().nextInt(COLORES.length)];
+                    } while (coloresUsados[colorAsignado - 1]);
+                    coloresUsados[colorAsignado - 1] = true;
+            
+                    panel.setColorCirculo(numeroCirculo, colorAsignado);
+                }//else
+            }
+
+
+}//run
+
+
+public static boolean todosSonTrue(boolean[] array) {
+    for (boolean elemento : array) {
+        if (!elemento) {
+            return false;
+        }
+    }
+    return true;
+}//todosSonTrue
+
 }
+
     
 
